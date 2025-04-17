47b702cba36006f32c6384020fb400f038d5f42d
Johnny Faris <jkfaris@gmail.com>
2025-04-17 16:36:52 -0700

    reorganized

---
+++ src/components/cards/EditCard.js
@@ -0,0 +1,7 @@
+// The Edit Card screen allows the user to modify information on an existing card.
+// The Edit Card screen has the following features:
+
+// The path to this screen should include the deckId and the cardId (i.e., /decks/:deckId/cards/:cardId/edit).
+// There is a breadcrumb navigation bar with a link to home /, followed by the name of the deck of which the edited card is a member, and finally the text Edit Card :cardId (e.g., Home / React Router / Edit Card 3).
+// It displays the same form as the Add Card screen, except it is prefilled with information for the existing card. It can be edited and updated.
+// If the user clicks on either Save or Cancel, the user is taken to the Deck screen.
