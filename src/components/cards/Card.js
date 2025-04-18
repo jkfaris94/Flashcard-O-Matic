d6722497f4a8f554b9998872bc3bda002c2527ff
Johnny Faris <jkfaris@gmail.com>
2025-04-18 16:36:31 -0700

    added function Card() using parameters passed from fetch in decks

--- src/components/cards/Card.js
+++ src/components/cards/Card.js
@@ -1,7 +1,10 @@
 import React from "react";
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 
 function Card ({ card, deckId }) {
+    const navigate = useNavigate();
+
+
     const handleDeleteCard = () => {
         const confirm = window.confirm("Delete this card?");
         if (confirm) {
@@ -17,17 +20,26 @@ function Card ({ card, deckId }) {
 
     return (
         <div className="card mb-3">
-            <div className="card-body">
-                <div className="row">
-                    <div className="col">{card.front}</div>
-                    <div className="col">{card.back}</div>
-                </div>
-                <Link to={`/decks/${deckId}/cards/${card.id}/edit`}>
-                    <button className="btn btn-secondary mr-2">Edit</button>
-                </Link>
-                <button onClick={handleDeleteCard} className="btn btn-danger">Delete</button>
+        <div className="card-body">
+          <div className="row">
+            <div className="col">
+              <p>{card.front}</p>
             </div>
+            <div className="col">
+              <p>{card.back}</p>
+            </div>
+          </div>
+          <div className="d-flex justify-content-end">
+            <Link to={`/decks/${deckId}/cards/${card.id}/edit`}>
+              <button className="btn btn-secondary mr-2">Edit</button>
+            </Link>
+            <button className="btn btn-danger" onClick={handleDeleteCard}>
+              Delete
+            </button>
+          </div>
         </div>
+      </div>
+  
     );
 }
 
