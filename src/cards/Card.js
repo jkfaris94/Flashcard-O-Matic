a07acc89a2a0afe2efc9b5098cb940502d2299de
Johnny Faris <jkfaris@gmail.com>
2025-04-21 21:50:20 -0700

    removed unused useNavigate

--- src/cards/Card.js
+++ src/cards/Card.js
@@ -1,9 +1,7 @@
 import React from "react";
-import { Link, useNavigate } from "react-router-dom"
+import { Link } from "react-router-dom"
 
 function Card ({ card, deckId }) {
-    // const navigate = useNavigate();
-
 
     const handleDeleteCard = () => {
         const confirm = window.confirm("Delete this card?");
