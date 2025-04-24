58f06d4c2153d10a9b0490f832036b8704db38f1
Johnny Faris <jkfaris@gmail.com>
2025-04-24 16:19:33 -0700

    adjusted Header

--- src/common/Header.js
+++ src/common/Header.js
@@ -1,9 +1,13 @@
 function Header () {
     return (
+        
         <div className="header">
-            <h1>Flashcard-o-matic</h1>
-            <p>Discover The Flashcard Difference</p>
+            <div className="container">
+                <h1>Flashcard-o-matic</h1>
+                <p>Discover The Flashcard Difference</p>
+            </div>
         </div>
+
     )
 }
 
